d237e0cc4bf1636641ef32a59d231b104881abfb
Mayank Sati <mayank.sati@gwmidc.in>
2020-06-10 15:13:07 +0530

    added ros2 package

--- example/test_math_func.py
+++
@@ -1,9 +0,0 @@
-import math_func
-class test_func:
-    def test_product(self):
-        assert False
-
-    def test_product(self):
-        assert math_func.product(5, 5) == 25
-        assert math_func.product(5) == 10
-        assert math_func.product(7) == 14
